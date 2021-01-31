90e3d25caef9c14aebb12cdf9f9c0e32a21ed162
myyrakle <sssang97@naver.com>
2021-01-31 20:23:09 +0900

    칵테일 조회 쿼리 구현

--- lib/sqlite/manager.dart
+++ lib/sqlite/manager.dart
@@ -27,4 +27,44 @@ class DatabaseManager {
       from tb_cocktail_method
     ''');
   }
+
+  // 칵테일 레시피 전체 조회
+  Future<List<Object>> getCocktailListAll(String search) async {
+    return await database.rawQuery('''
+      select *
+      from tb_cocktail a
+      where 1=1
+        a.cocktail_name like '%$search%'
+      order by a.cocktail_name
+    ''');
+  }
+
+  // 특정 재료를 사용하는 칵테일 레시피 전체 조회
+  Future<List<Object>> getCocktailListByMaterial(String materialId) async {
+    return await database.rawQuery('''
+      select *
+      from tb_cocktail a
+      where 1=1
+        and exists 
+        (
+          select 1 
+          from tb_cocktail_material b
+          where 1=1
+            and a.cocktail_no = b.cocktail_no
+            and b.material_id = '$materialId'
+        )
+      order by a.cocktail_name
+    ''');
+  }
+
+  // 특정 재료를 사용하는 칵테일 레시피 전체 조회
+  Future<List<Object>> getCocktailListByMethod(String materialId) async {
+    return await database.rawQuery('''
+      select *
+      from tb_cocktail a
+      where 1=1
+        a.cocktail_method = '$materialId'
+      order by a.cocktail_name
+    ''');
+  }
 }
